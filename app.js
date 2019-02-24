86c31140bea6894da2438c793f1fdf7496efe51e
Sayed Abdul Karim <43297672+sayedabdulkarim@users.noreply.github.com>
2019-02-24 11:21:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(passport.session());
 //connect flash
 app.use(flash())
 
-//GLOBAL VAR 
+//GLOBAL VAR for flash message
 app.use((req, res, next) => {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
